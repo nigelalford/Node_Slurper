0cfa52aa8e8ee6aaaa9713c5ad8bd1cdd7c21590
Nigel A <nigel.alford2@nscorp.com>
2014-04-02 07:48:25 -0400

    updated package.json to include 'mkdirp' module

---
+++ test/main.js
@@ -0,0 +1,9 @@
+var should = require('chai').should(),
+	esri = require('../main'),
+	getAMD = esri.getEsriAMD;
+
+(5).should.be.exactly(5).and.be.a.Number;
+
+describe('#getAMD', function(){
+	//getAMD.should.have.property('name', 'homer');
+});
