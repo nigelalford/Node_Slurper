0e0c6df5b253d2e84e27930080a65cee95909347
Nigel A <nigel.alford2@nscorp.com>
2014-05-15 21:59:57 -0400

    removed unused js files and changed file to 'index.js'

--- index.js
+++ index.js
@@ -1,49 +1,53 @@
+// Dependencies
 var fs = require('fs'),
-    http = require('http'),
-    /*url = require('url'),
+	http = require('http'),
+	mkdirp = require('mkdirp'),
+	/*url = require('url'),
     exec = require('child_process').exec,
     spawn = require('child_process').spawn,*/
-    mkdirp = require('mkdirp'),
-    esriAMD = require('./esri_3.8_modules');
+	esriAMD = require('./esri_3.8_modules');
 
 // App variables
-var file_url = 'http://js.arcgis.com/3.8amd/js/esri/';
-    //setDir = './esri3.8amd/'
+var file_url = 'http://js.arcgis.com/3.8amd/js/esri/',
+	setDir = './newFolderYo/';
 
-var scrape = function (location) {
-	console.log('scraping...');
-    var readFile = function (callback) {
-        if (esriAMD.length > 0) {
-            var setFile = esriAMD.shift(),
-                fileEnding = setFile.split('/').pop(),
-                folders = setFile.substr(0, setFile.lastIndexOf('/')) + '/';
+/*(function clearFiles() {
+	fs.rmdir(setDir, function (err) {
+		if (err) throw err;
+	});
+})();*/
 
-            mkdirp(location + folders, function (err) {
-                var file = fs.createWriteStream(location + folders + fileEnding);
-                if (err) {
-                    console.log('prob: ' + err);
-                }
+function readFile(callback) {
+	if (esriAMD.length > 0) {
+		var setFile = esriAMD.shift(),
+			fileEnding = setFile.split('/').pop(),
+			folders = setFile.substr(0, setFile.lastIndexOf('/')) + '/';
 
-                http.get(file_url + setFile, function (res) {
-                    res.on('error', function (err) {
-                        console.log(err);
-                    });
-                    res.on('data', function (data) {
-                        file.write(data);
-                    });
-                    res.on('end', function () {
-                        readFile(callback);
-                    });
-                });
+		mkdirp(setDir + folders, function (err) {
+			var file = fs.createWriteStream(setDir + folders + fileEnding);
+			if (err) {
+				console.log('prob: ' + err);
+			}
 
-            });
+			http.get(file_url + setFile, function (res) {
+				res.on('error', function (err) {
+					//debugger;
+					console.log(err);
+				});
+				res.on('data', function (data) {
+					file.write(data);
+				});
+				res.on('end', function () {
+					readFile(callback);
+				});
+			});
 
-        } else {
-            callback();
-        }
-        console.log("scraping finished");
-    };
-    return readFile;
-};
+		});
 
-module.exports = scrape(location);
+	} else {
+		callback();
+	}
+}
+readFile(function () {
+	console.log("reading finishes");
+});
